dfe422bdeddad8eaaf1204a0576dcb21498a6962
toannd <sitdownrightnow.dev@gmail.com>
2020-11-13 08:56:53 +0700

    global lang

---
+++ src/io/lana/sqlstarter/app/LanguageBundleProvider.java
@@ -0,0 +1,23 @@
+package io.lana.sqlstarter.app;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LanguageBundleProvider {
+    private static Locale locale = new Locale("en", "US");
+
+    private LanguageBundleProvider() {
+    }
+
+    public static ResourceBundle getBundle(String path) {
+        return ResourceBundle.getBundle(path, locale);
+    }
+
+    public static void setLocale(Locale locale) {
+        LanguageBundleProvider.locale = locale;
+    }
+
+    public static void setLocale(String lang, String country) {
+        setLocale(new Locale(lang, country));
+    }
+}
